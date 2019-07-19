560bc299e5b96d3f0d8d303254750fca77527533
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-19 12:18:55 -0700

    added Christmas and order size conditionals

--- melons.py
+++ melons.py
@@ -12,6 +12,10 @@ class AbstractMelonOrder():
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species == "Christmas":
+            base_price = base_price * 1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -43,3 +47,12 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        total = super().get_total()
+
+        if self.qty < 10:
+            total += 3
+
+        return total
